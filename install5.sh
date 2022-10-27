1af4e0a9e51f41dd6f865c9408fe9f9be93e88f8
markusplayzz <65229701+markusplayzz@users.noreply.github.com>
2022-10-28 00:24:57 +0200

    Solved echo problem

--- install5.sh
+++ install5.sh
@@ -1,7 +1,7 @@
 # Only UEFI
 
 echo "exec \"setxkbmap -layout us,de\"" >> ~/.config/i3/config
-echo "exec \"setxkbmap -option \'grp:alt_shift_toggle\'\"" >> ~/.config/i3/config
+echo "exec \"setxkbmap -option 'grp:alt_shift_toggle'\"" >> ~/.config/i3/config
 
 # Delete installation scripts
 sudo rm /root/install3.sh
