ca189829375bd73bad04c6a46f43fc143661b467
markusplayzz <65229701+markusplayzz@users.noreply.github.com>
2022-10-28 11:00:36 +0200

    Update install4.sh

--- install4.sh
+++ install4.sh
@@ -2,8 +2,10 @@
 
 echo "exec \"setxkbmap -layout us,de\"" >> ~/.config/i3/config
 echo "exec \"setxkbmap -option 'grp:alt_shift_toggle'\"" >> ~/.config/i3/config
+exec setxkbmap -layout us,de
+exec setxkbmap -option 'grp:alt_shift_toggle'
 
 # Delete installation scripts
-sudo rm /root/install3.sh
+sudo rm /root/install2.sh
+rm ~/install3.sh
 rm ~/install4.sh
-rm ~/install5.sh
