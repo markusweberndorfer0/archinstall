dc90b69c7e00830dd8cc3f79a3a485881cf948e7
markusplayzz <65229701+markusplayzz@users.noreply.github.com>
2022-10-28 12:37:36 +0200

    Added i3lock

--- install4.sh
+++ install4.sh
@@ -9,6 +9,7 @@ echo "exec \"setxkbmap -layout us,de\"" >> ~/.config/i3/config
 echo "exec \"setxkbmap -option 'grp:alt_shift_toggle'\"" >> ~/.config/i3/config
 exec setxkbmap -layout us,de
 exec setxkbmap -option 'grp:alt_shift_toggle'
+bindsym $mod+x exec i3lock
 
 # Delete installation scripts
 sudo rm /root/install2.sh
